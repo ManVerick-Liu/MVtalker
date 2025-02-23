344120a1ea95fa6fac1db9e9e908ccdebd78f407
ManVerick Liu <soldier_lqy@hotmail.com>
2025-02-23 22:46:51 +0800

    v0.0.3 Sentinel引入与配置

--- Utilities/src/main/java/com/mvtalker/utilities/feign/UserFeignClient.java
+++ Utilities/src/main/java/com/mvtalker/utilities/feign/UserFeignClient.java
@@ -1,8 +1,9 @@
 package com.mvtalker.utilities.feign;
 
+import com.mvtalker.utilities.feign.fallback.UserFeignClientFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "user-service")
+@FeignClient(name = "user-service", fallbackFactory = UserFeignClientFallback.class)
 public interface UserFeignClient
 {
 
---
+++ Utilities/src/main/java/com/mvtalker/utilities/feign/fallback/UserFeignClientFallback.java
@@ -0,0 +1,18 @@
+package com.mvtalker.utilities.feign.fallback;
+
+import com.mvtalker.utilities.feign.UserFeignClient;
+import org.springframework.cloud.openfeign.FallbackFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserFeignClientFallback implements FallbackFactory<UserFeignClient>
+{
+    @Override
+    public UserFeignClient create(Throwable cause)
+    {
+        return new UserFeignClient()
+        {
+            // 编写调用失败的逻辑
+        };
+    }
+}
